a3edb955abb06c22068f7cc71e5ce93d6beecd39
Aaron Bustos <aaronj.bustos@gmail.com>
2023-02-26 20:36:12 -0600

    cambios

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -8,7 +8,10 @@ import com.tienda.service.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+
 
 @Controller
 public class PersonaController {
